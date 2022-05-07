ae7466884289328101e48221645003f31b793200
RozannevdBerk <r.m.v.d.berk@gmail.com>
2022-05-07 17:58:54 +0200

    added Nick's heuristic but performance still bad :(

--- Discrete-Simulations/robot_configs/policy_iteration_attempt.py
+++ Discrete-Simulations/robot_configs/policy_iteration_attempt.py
@@ -2,9 +2,13 @@ import numpy as np
 from random import choices
 
 '''
-Update 6 May: We worked out a lot of the bugs. Nigel now works decently on some maps though we haven't
-had time for extensive testing. It is hard to say whether the flaws in this implementation are
-due to the implementation itself or due to policy iteration just not being that good.
+Update 7 May: Included Nick's heuristics for the reward values. The performance is still not
+great and it seems to be caused by certain tiles around the robot's current tile being assigned
+a value of 0 when they shouldn't be. I thought this was caused by something going wrong during
+the policy optimization step, as it seemed to happen most often after the third round of policy
+optimization. However, capping policy optimization to no more than three only helped a little.
+Yasemin and I are both stuck, we've only been able to conclude that whatever is happening is weird.
+Pls help.
 '''
 
 #TO IMPLEMENT: global variable(s) so we remember Values and policies between epochs, for speed
@@ -43,7 +47,7 @@ def robot_epoch(robot):
         
     #keep track of clean and dirty tiles:
     world_state = np.zeros((*V.shape,3)).astype(bool)
-    world_state[:,:,0] = (current_world==0) #clean
+    world_state[:,:,0] = (current_world==0)|(current_world==-3) #clean/current position
     world_state[:,:,1] = (current_world==1) #dirty
     world_state[:,:,2] = (current_world==-1)|(current_world==-2) #wall/obstacle, -3 is robot's current location so can't use <0
     
@@ -51,22 +55,55 @@ def robot_epoch(robot):
     
     #start with 25% chance for a move in each direction, arbitrary policy
     policy_map = np.full(shape=(*V.shape,4),fill_value=0.25)
-    theta = 5
-    gamma = 0.5
+    theta = 10
+    gamma = 1
     
     opt_policy = False
-    it = 0
+    pol_it = 0
     #let's start iterating:
-    while opt_policy==False and it < 25:
+    while opt_policy==False and pol_it < 2:
         #print("still haven't found optimal policy")
         #fill Value grid with raw rewards as a starting point
         for i in range(3):
             #print("filling V")
             V[world_state[:,:,i]] = rewards[i]
+            
+        #Alter rewards based on proximity to walls/clean tiles
+        for x in range(V.shape[0]):
+            for y in range(V.shape[1]):
+                if world_state[x,y,2] == True: #ignore walls, keep initial rewards
+                    continue
+                lst_neighbor=[]
+                count_walls = 0; count_neighbor = 0
+                for i in range(4):
+                #for dir in moves_actual:
+                    #dir = moves_actual[i]
+                    new_pos = update_cor(x,y,moves[i])
+                    if world_state[new_pos][2] == False: #if new position isn't a wall
+                        #lst_dir.append(moves[i])
+                        if world_state[new_pos][0] == True and world_state[x,y,1] == True: #Reward for nearby clean tiles
+                            V[x,y] += 0.2*V[x,y]
+                    elif world_state[x,y,1] == True: #if current tile is dirty
+                        if world_state[new_pos][2] == True: #Rewards for being nearby to boundaries of obstacles
+                            V[x,y] += 0.2*V[x,y]
+                            count_neighbor += 1
+                            count_walls += 1
+                            lst_neighbor.append(moves[i])
+                        if world_state[new_pos][0] == True:
+                            count_neighbor += 1
+
+                if count_walls == 2 and (('w' in lst_neighbor and 'e' in lst_neighbor) or ('n' in lst_neighbor and 's' in lst_neighbor)): #If there are two walls next to eachother than treat as door
+                    V[x,y] -= 0.1*V[x,y]
+                    #print('-'*20)
+                #If it has no dirty tiles around, prioritize
+                if count_neighbor == 4:
+                    V[x,y] += 0.7*V[x,y]
+            
+            
         biggest_dif = 1000
-        it2 = 0
-        while biggest_dif > theta and it2 < 25:
-            print(f"still iterating, biggest dif is {biggest_dif}")
+        V_it = 0
+        while biggest_dif > theta and V_it < 25:
+            #print(f"still iterating, biggest dif is {biggest_dif}")
             
             max_dif_so_far = 0
             for x in range(V.shape[0]):
@@ -85,9 +122,9 @@ def robot_epoch(robot):
                         V_new += policy_map[x,y,i] * G
                     V[x,y] = V_new
                     max_dif_so_far = max(max_dif_so_far, abs(V_new-V_old))
-                    
-            it2 += 1
-                    
+            
+            #print(V)
+            V_it += 1
                 
             #update biggest_dif so the loop won't run forever
             biggest_dif = max_dif_so_far
@@ -117,12 +154,17 @@ def robot_epoch(robot):
                     best_moves = pm_one_hot.astype(int)
                     
                 new_policy = best_moves / np.sum(best_moves) #turn into probabilities
-                #print(f"{x},{y}: old: {old_policy}, new: {new_policy}, best: {best_moves}")
+                # if (x,y)==current_pos:
+                #     print(f"{x},{y}: old: {old_policy}, new: {new_policy}, it: {pol_it}")
+                
                 if (old_policy != new_policy).any(): #if the policy needs to be changed
                     opt_policy = False
                     policy_map[x,y,:] = new_policy #update policy
                     
-        it += 1
+        pol_it += 1
+        for i in range(4):
+            neigh_pos = update_cor(*current_pos, moves[i])
+            print(f"{neigh_pos[0]},{neigh_pos[1]}: {policy_map[neigh_pos]}, iteration: {pol_it}")
                 
     
     #choose move randomly based on the probabilities in the policy_map
