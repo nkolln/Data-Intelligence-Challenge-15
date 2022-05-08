623478413e4601edf9dddf04238245e966c27b62
RozannevdBerk <r.m.v.d.berk@gmail.com>
2022-05-08 22:00:01 +0200

    added heatmap functionality

--- Discrete-Simulations/plotting.py
+++ Discrete-Simulations/plotting.py
@@ -1,3 +1,4 @@
+import numpy as np
 import matplotlib.pyplot as plt
 import json
 from statistics import mean
@@ -21,26 +22,40 @@ def calc_overall_avg_eff(data, robot_name):
     avg = avg/(12*4)
     return avg
 
+
 def plot_efficiencies(data, robot_name, grid_name):
+    
+    a = np.zeros((3,4))
 
     tg_pairs = data.get(robot_name).get(grid_name).keys()
 
     tg_avg_eff = [mean(data.get(robot_name).get(grid_name).get(tg).get("efficiencies")) for tg in tg_pairs]
 
-    tg_pairs_trimmed = [tg.replace("theta ", "").replace("gamma ", "") for tg in tg_pairs]
-    plt.plot(tg_pairs_trimmed, tg_avg_eff)
+    print(tg_avg_eff)
+
+    #tg_pairs_trimmed = [tg.replace("theta ", "").replace("gamma ", "") for tg in tg_pairs]
+    
+    for i in range(12):
+        x = i//4
+        y = i%4
+        a[x,y] = tg_avg_eff[i]
+    
+    plt.imshow(a, cmap='hot')
     plt.title(grid_name)
-    plt.xlabel("theta-gamma pairs")
-    plt.ylabel("average efficiency")
+    plt.ylabel("theta")
+    plt.yticks([0,1,2], ['0.2', '1.0', '5.0'])
+    plt.xlabel("gamma")
+    plt.xticks([0,1,2,3], ['0.2','0.5','0.8','1.0'])
+    plt.colorbar()
     plt.show()
 
 
 
-with open("results.json") as file:
+with open("PI_results.json") as file:
     data = json.load(file)
-print("overall eff: ", calc_overall_avg_eff(data, "VI"))
+print("overall eff: ", calc_overall_avg_eff(data, "PI"))
 
-for grid in data.get("VI").keys():
-    plot_efficiencies(data, "VI", grid)
+for grid in data.get("PI").keys():
+    plot_efficiencies(data, "PI", grid)
 
 
