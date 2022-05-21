53348aca5d67f9e01c54ae2ed8f87d1190e01a97
ArdaGunay99 <arda.99.gunay@gmail.com>
2022-05-21 16:32:46 +0200

    implemented random starting positions for more exploration

--- Discrete-Simulations/MC_test.py
+++ Discrete-Simulations/MC_test.py
@@ -8,22 +8,19 @@ import os
 from environment import Robot
 
 import robot_configs.MonteCarlo as MC
+# get the grid
+with open(f'test_grids/custom2_corridor.grid', 'rb') as f:
+    grid = pickle.load(f)
+# initialize the robot
+robot = Robot(grid, (1, 1), orientation='n', battery_drain_p=0.1, battery_drain_lam=2)
+# train the robot
+policy = MC.train(robot, episodes=10000, epsilon=0.1)
 
+# re-initialize the robot
 with open(f'test_grids/1example-random-house-0.grid', 'rb') as f:
     grid = pickle.load(f)
-
-robot = Robot(grid, (1, 1), orientation='n', battery_drain_p=0.25, battery_drain_lam=2)
-
-policy = MC.train_monte_carlo_e_soft(robot, episodes=10000)
-
-with open(f'test_grids/1example-random-house-0.grid', 'rb') as f:
-    grid = pickle.load(f)
-
-
-
-
-robot = Robot(grid, (1, 1), orientation='n', battery_drain_p=0.25, battery_drain_lam=2)
-
-print("result of testing: ", MC.test_policy(robot, policy, iter_count=300))
+# test the efficiency of the policy
+robot = Robot(grid, (1, 1), orientation='n', battery_drain_p=0.1, battery_drain_lam=2)
+print("result of testing: ", MC.test_pi(robot, policy, iter_count=300))
 
 
--- Discrete-Simulations/robot_configs/MonteCarlo.py
+++ Discrete-Simulations/robot_configs/MonteCarlo.py
@@ -1,23 +1,16 @@
 import copy
 import operator
 import random
-
 import numpy as np
 from statistics import mean
 
+
+# converts the indices of a cell to its order number. e.g. in a 5x5 grid, (0,0) is 0th state and (1,0) is the 5th state
 def convert_cell_to_state(cell: tuple, grid: np.ndarray):
     shape = grid.shape
     column_count = shape[1]
     state = (cell[1]) + (cell[0]*column_count)
     return state
-
-
-# grid = np.zeros((5, 5))
-# print(grid)
-# for row in range(5):
-#     for column in range(5):
-#         grid[row, column] = convert_cell_to_state((row, column), grid)
-# print(grid)
 
 
 # returns a random policy dictionary with each action having equal probabilities
@@ -52,7 +45,7 @@ def init_Q(policy: dict):
 
     return Q
 
-
+# calculates the current rewards for all the states and makes the given move
 def turn_and_move(robot, move: tuple):
     moves = ['n', 'e', 's', 'w']
     moves_actual = [(0, -1), (1, 0), (0, 1), (-1, 0)]
@@ -60,9 +53,7 @@ def turn_and_move(robot, move: tuple):
     current_pos = robot.pos
     shape_w = current_world.shape
     rewards = np.copy(current_world)
-    # current_world[current_pos] = 0
 
-    # bounds = [[current_pos[0], current_pos[0]+shape_w[0]],[current_pos[1], current_pos[1]+shape_w[1]]]
     valid_states = []
     lst_end = []
     lst_goal = []
@@ -95,10 +86,10 @@ def turn_and_move(robot, move: tuple):
         """if current_world[coord]==0: #if its clean
             rewards[coord] = -0.5"""
         for i in range(4):
-            # for dir in moves_actual:
+
             dir = moves_actual[i]
             new_loc = tuple(map(operator.add, coord, dir))
-            if new_loc in valid_states:  # ((new_loc not in lst_taboo) and (new_loc not in end_states)):# and (new_loc[0] in range(bounds[0][0],bounds[0][1]))and(new_loc[1] in range(bounds[1][0],bounds[1][1]))):
+            if new_loc in valid_states:
                 lst_dir.append(moves[i])
                 if new_loc in lst_clean and current_world[coord] == 1 or current_world[
                     coord] == 2:  # Reward for nearby clean tiles
@@ -110,19 +101,15 @@ def turn_and_move(robot, move: tuple):
                     rewards[coord] += 1
                 if new_loc in lst_clean or new_loc in lst_taboo or new_loc in lst_wall:
                     count_neighbor += 1
-                # elif current_world[coord]==1:
                 if new_loc in lst_taboo:  # Logic to find if there is a doorway
                     count_walls += 1
                     lst_neighbor.append(dir)
         if count_walls == 2 and tuple(map(operator.add, lst_neighbor[0], lst_neighbor[1])) == (
-        0, 0):  # If there are two walls next to eachother than treat as door
+        0, 0):  # If there are two walls next to each other then treat as door
             rewards[coord] -= 1
-            # print('-'*20)
         # If it has no dirty tiles around, prioritize
         if count_neighbor == 4:
             rewards[coord] += 20
-        # if len(lst_dir) > 0:
-        #     actions.update({coord: lst_dir})
 
     statistics = {"clean": 0,
                   "dirty": 0,
@@ -131,13 +118,11 @@ def turn_and_move(robot, move: tuple):
     # Orient ourselves towards the dirty tile:
     while new_orient != robot.orientation:
         # If we don't have the wanted orientation, rotate clockwise until we do:
-        # print('Rotating right once.')
         robot.rotate('r')
     # Move:
     robot.move()
     reward = rewards[robot.pos]
-    if not robot.alive:
-        return robot, reward, statistics, True
+
     # Calculate some statistics:
     clean = (current_world == 0).sum()
     dirty = (current_world >= 1).sum()
@@ -147,26 +132,32 @@ def turn_and_move(robot, move: tuple):
                        "goal": goal})
     # Calculate the cleaned percentage:
     clean_percent = (clean / (dirty + clean)) * 100
-    # See if the room can be considered clean, if so, stop the simulaiton instance:
+    # return if robot died
+    if not robot.alive:
+        return robot, reward, clean_percent, True
+    # See if the room can be considered clean, if so, end the episode:
     if clean_percent >= 100 and goal == 0:
-        return robot, reward, statistics, True
+        return robot, reward, clean_percent, True
 
-    return robot, reward, statistics, False
+    return robot, reward, clean_percent, False
 
 
+# calculates the efficiency of an episode by looking at the revisited tiles
 def calculate_episode_efficiency(robot):
     n_total_tiles = ((robot.grid.cells >= 0) & (robot.grid.cells < 3)).sum()
     moves = [(x, y) for (x, y) in zip(robot.history[0], robot.history[1])]
-    u_moves = set(moves)
-    n_revisted_tiles = len(moves) - len(u_moves)
-    efficiency = (100 * n_total_tiles) / (n_total_tiles + n_revisted_tiles)
+    u_moves = set(moves)  # unique moves
+    n_revisited_tiles = len(moves) - len(u_moves)
+    efficiency = (100 * n_total_tiles) / (n_total_tiles + n_revisited_tiles)
     return efficiency
 
+
+#  runs a single episode with the given policy
 def run_episode(robot, policy: dict):
     grid = robot.grid.cells
     episode = []
     done = False
-
+    clean_percent = 0
     while not done:
         # state-action-reward for a step
         sar = []
@@ -174,55 +165,67 @@ def run_episode(robot, policy: dict):
         sar.append(state)
 
         n = random.uniform(0, sum(policy[state].values()))
-        top_range = 0
+        top = 0
+        # select an action to make
         for probability in policy[state].items():
-            top_range += probability[1]
-            if n < top_range:
+            top += probability[1]
+            if n < top:
                 action = probability[0]
                 break
-
-        robot, reward, statistics, done = turn_and_move(robot, action)
+        # make a step and get the reward for it
+        robot, reward, clean_percent, done = turn_and_move(robot, action)
 
         sar.append(action)
         sar.append(reward)
         episode.append(sar)
     efficiency = calculate_episode_efficiency(robot)
-    return episode, efficiency
+    return episode, efficiency, clean_percent
 
 
-def test_policy(robot, policy, iter_count):
-    wins = 0,
+#  runs the given policy iter_count times to get the average efficiency
+def test_pi(robot, policy, iter_count):
     efficiencies = []
     for i in range(iter_count):
         print("iter ", i)
-        episode, efficiency = run_episode(robot, policy)
+        episode, efficiency, clean = run_episode(robot, policy)
         efficiencies.append(efficiency)
 
     return mean(efficiencies)
 
+# returns the indices of all the dirty cells of the grid as tuples
+def get_dirty_cells(robot):
+    grid = robot.grid.cells
+    dirty_tiles = np.argwhere(grid >= 0)
+    dirty_tiles_tuples = []
+    for i in range(len(dirty_tiles)):
+        dirty_tiles_tuples.append(tuple(dirty_tiles[i]))
+    # print(type(dirty_tiles_tuples[0]))
+    return dirty_tiles_tuples
 
-def train_monte_carlo_e_soft(robot, episodes=100, policy=None, epsilon=0.01):
+
+def train(robot, episodes=100, policy=None, epsilon=0.9):
     if not policy:
         policy = init_random_pi(robot)  # Create an empty dictionary to store state action values
     Q = init_Q(policy)  # Empty dictionary for storing rewards for each state-action pair
-    returns = {}  # 3.
+    returns = {}
 
     for _ in range(episodes):  # Looping through episodes
-        test_robot = copy.deepcopy(robot)
-        # print("episode ", _)
-        G = 0  # Store cumulative reward in G (initialized at 0)
-        episode, efficiency = run_episode(robot=test_robot, policy=policy)  # Store state, action and value respectively
-        print("episode ", _, "efficiency: ", efficiency)
-        # for loop through reversed indices of episode array.
-        # The logic behind it being reversed is that the eventual reward would be at the end.
-        # So we have to go back from the last timestep to the first one propagating result from the future.
+        test_robot = copy.deepcopy(robot) # work on a copy of the robot and the grid
 
+        dirty_tiles = get_dirty_cells(test_robot)  # get the dirty cells in the grid
+        test_robot.pos = random.choice(dirty_tiles)  # choose a random starting position to increase exploration
+
+        G = 0  # cumulative reward
+        episode, efficiency, clean = run_episode(robot=test_robot, policy=policy)   # run 1 episode
+        print("episode ", _, "efficiency: ", efficiency, " cleaned: ", clean, "%")
+
+        #loop through the episode in reverse order
         for i in reversed(range(0, len(episode))):
             s_t, a_t, r_t = episode[i]
             state_action = (s_t, a_t)
-            G += r_t  # Increment total reward by reward on current timestep
+            G += r_t  # Increment total reward by reward on current step
 
-            if not state_action in [(x[0], x[1]) for x in episode[0:i]]:  #
+            if not state_action in [(x[0], x[1]) for x in episode[0:i]]:
                 if returns.get(state_action):
                     returns[state_action].append(G)
                 else:
@@ -234,7 +237,7 @@ def train_monte_carlo_e_soft(robot, episodes=100, policy=None, epsilon=0.01):
                 indices = [i for i, x in enumerate(Q_list) if x == max(Q_list)]
                 max_Q = random.choice(indices)
 
-                A_star = max_Q  # 14.
+                A_star = max_Q
 
                 for a in policy[s_t].items():  # Update action probability for s_t in policy
                     if a[0] == A_star:
